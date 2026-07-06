afb704ac1b699f8d304fe723999844f5ffa1f917
Kodchakan Prajob <kodchakan.p@cimbthail.com>
2026-07-06 13:25:04 +0700

    Initial Commit

---
+++ ScoreCounter Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScoreCounter Watch App
+//
+//  Created by Kodchakan Prajob on 6/7/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScoreCounter Watch App/ScoreCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreCounterApp.swift
+//  ScoreCounter Watch App
+//
+//  Created by Kodchakan Prajob on 6/7/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct ScoreCounter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
